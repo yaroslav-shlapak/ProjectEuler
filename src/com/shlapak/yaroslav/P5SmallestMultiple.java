a6ff312da50501f735173b0d21d4b4185055827c
yaroslav <y.shlapak@samsung.com>
2016-02-25 21:01:40 +0200

    5th problem was added

---
+++ src/com/shlapak/yaroslav/P5SmallestMultiple.java
@@ -0,0 +1,12 @@
+package com.shlapak.yaroslav;
+
+import java.util.LinkedList;
+
+/**
+ * Created by yarl on 25.02.16.
+ */
+public class P5SmallestMultiple {
+    public static void main(String[] args) {
+        System.out.println(5 * 7 * 9 * 11 * 13 * 16 * 17 * 19);
+    }
+}
